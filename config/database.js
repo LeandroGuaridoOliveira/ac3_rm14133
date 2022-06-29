70179585f0423ddec63623460f847cd624aebff5
LeandroGuaridoOliveira <84139383+LeandroGuaridoOliveira@users.noreply.github.com>
2022-06-29 08:18:05 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 //script de conexao
 const conn = async()=>{
-    const atlas = await mongoose.connect('mongodb+srv://UserAdmin:rm14133@ac1-2tri.xb0bm.mongodb.net/test')
+    const atlas = await mongoose.connect('mongodb+srv://UserAdmin:14133@ac1-2tri.xb0bm.mongodb.net/test')
 }
 
 //exportar as informações para acesso externo
